fa976e22e00073ec882126e765e442bcd4e191ec
VladimirGalajko <132641444+VladimirGalajko@users.noreply.github.com>
2023-12-29 21:12:30 +0000

    Deploying to gh-pages from @ VladimirGalajko/goit-react-hw-08-phonebook@88db3884ce6c796e97e7f245915624147cb26d8e 🚀

---
+++ static/js/210.e843e0c6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[210],{5210:function(e,t,r){r.r(t);var n=r(3329);t.default=function(){return(0,n.jsx)(n.Fragment,{children:(0,n.jsx)("p",{children:"Welcome to Phonebook!"})})}}}]);
+//# sourceMappingURL=210.e843e0c6.chunk.js.map
